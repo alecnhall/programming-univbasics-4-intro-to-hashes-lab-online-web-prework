df17d109edc9158cd0dc257e730eedc304f0443a
Alec Hall <alecnhall@gmail.com>
2019-07-23 20:07:57 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,8 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash = { }
+  hash[key] = value
 end
 
 def read_from_hash(hash, key)
